8a50c3e0572b76def5c6a2a74f4309929cd13ba4
Mihnea Simian <contact@mesimian.com>
2012-06-01 11:55:50 +0300

    works with added files to repo

--- README.rst
+++ README.rst
@@ -1,4 +1,4 @@
-GitDiff2SvnDiff v0.1
+GitDiff2SvnDiff v0.2
 ====================
 
 This is a small python script that converts a given git diff file into a
@@ -8,15 +8,19 @@ Usage
 =====
 
 Input arguments::
-  path_to_git_diff_file svn_revision_number [prefix_to_append]
+  python gitdiff2svndiff.py svn_revision_number [prefix_to_append]
+
+Shortcut for last commit ("git show"-diff to svn-diff):
+  ./gitsvnshow
+OBS!: Only works with git-svn repos and if your HEAD^ commit is already on svn.
 
 Where::
   * svn_revision - revision number on svn for older version of the files
-  * prefix_to_append - optional, you may choose to append a path to each 
- file path
+  * prefix_to_append - optional, you may choose to append a path to each
+    file path
 
 Limitations
 ===========
 
-It shouldn't work with diffs containing file creations/deletions.
+It shouldn't work with diffs containing file deletions.
 This should be the next version.
--- gitdiff2svndiff.py
+++ gitdiff2svndiff.py
@@ -2,8 +2,8 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        sys.exit("Usage: python %program git_diff_file svn_revision_number"
-                 " [path_prefix_to_append]")
+        sys.exit("Usage: python %s svn_revision_number"
+                 " [path_prefix_to_append]" % argv[0])
     prefix = ""
     if len(sys.argv) == 4:
         prefix = sys.argv[3]
@@ -11,23 +11,31 @@ if __name__ == "__main__":
             prefix += "/"
     revision = int(sys.argv[2])
     git_diff = open(sys.argv[1], "r")
+    new_file_flag = False
     for line in git_diff:
+        if line.startswith("new file mode"):
+            continue # skip
         if line.startswith("diff --git a/"):
             chunks = line.split(" ")
             sys.stdout.write("Index: ")
             sys.stdout.write(prefix + chunks[2][chunks[2].find("/")+1:] + "\n")
         elif line.startswith("index "):
             sys.stdout.write(("================================"
-                   "===================================\n"))
+                              "===================================\n"))
         elif line.startswith("--- a/"):
             line = line[:-1]
             sys.stdout.write(line.replace(" a/", " "+prefix))
             sys.stdout.write("\t(revision %d)\n" % revision)
         elif line.startswith("+++ b/"):
-            line = line[:-1]
-            sys.stdout.write(line.replace(" b/", " "+prefix))
+            line = line[:-1].replace(" b/", " "+prefix)
+            if new_file_flag:
+                sys.stdout.write(line.replace("+++ ", "--- "))
+                sys.stdout.write("\t(revision 0)\n")
+                new_file_flag = False
+            sys.stdout.write(line)
             sys.stdout.write("\t(working copy)\n")
+        elif line.startswith("--- /dev/null"):
+            new_file_flag = True
         else:
             sys.stdout.write(line)
     git_diff.close()
-
